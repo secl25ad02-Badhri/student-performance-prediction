3285154450472a714087562a953a0fb15528b7c7
Badhrinarayanan R K <badhrirk@gmail.com>
2026-06-17 21:43:24 +0530

    Added user input for prediction

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("student_data.csv")
 
-plt.scatter(df["Study_Hours"], df["Marks"])
+X = df[["Study_Hours"]]
+y = df["Marks"]
 
-plt.xlabel("Study Hours")
-plt.ylabel("Marks")
-plt.title("Student Performance Analysis")
+model = LinearRegression()
+model.fit(X, y)
 
-plt.show()
+hours = float(input("Enter study hours: "))
+
+prediction = model.predict(
+    pd.DataFrame([[hours]], columns=["Study_Hours"])
+)
+
+print(f"Predicted Marks: {prediction[0]:.2f}")
